d2992aefa0eadd3da3d8a25b1d5d844a99240479
950207 <jungwon7769@gmail.com>
2018-12-16 19:00:08 +0900

    SaveManager 추가.

---
+++ ProjectB/Assets/02.Scripts/Game/Save/AccountData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class AccountData : MonoBehaviour
+{
+    string StoreName;
+    int Money;
+    int Cash;
+    uint PlayTime;
+    
+    // ++ 재료 정보들..
+    
+    public string GetStoreName()
+    {
+        return StoreName;
+    }
+
+    public int GetMoney()
+    {
+        return Money;
+    }
+
+    public int GetCash()
+    {
+        return Cash;
+    }
+
+    public uint GetPlayTime()
+    {
+        return PlayTime;
+    }
+
+
+    // Set
+    public void SetStoreName(string InStoreName)
+    {
+        StoreName = InStoreName;
+    }
+
+    public void SetMoney(int InMoney)
+    {
+        Money = InMoney;
+    }
+
+
+    public void SetCash(int InCash)
+    {
+        Cash = InCash;
+    }
+
+    public void SetPlayTime(uint InPlayTime)
+    {
+        PlayTime = InPlayTime;
+    }
+
+}
